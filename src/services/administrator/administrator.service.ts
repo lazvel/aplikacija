3b8dfbb798bf353076d72fbb6c6240bda50e9550
Lazar Velimirovic <lazarvelimirovic43@gmail.com>
2020-08-28 20:56:28 +0200

    Added all entites from database

--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Administrator } from 'entities/administrator.entity';
+
 import { Repository } from 'typeorm';
+import { Administrator } from '../../../entities/administrator.entity';
 
 @Injectable()
 export class AdministratorService {
