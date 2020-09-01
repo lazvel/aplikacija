9d36df94f35890a237a920be7f3aa909354c2316
Lazar Velimirovic <lazarvelimirovic43@gmail.com>
2020-09-01 15:32:24 +0200

    delete js files

--- src/dtos/adminstrator/edit.adminstrator.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EditAdminstratorDto = void 0;
-class EditAdminstratorDto {
-}
-exports.EditAdminstratorDto = EditAdminstratorDto;
-//# sourceMappingURL=edit.adminstrator.dto.js.map
