3c623ed53b0264d88a2c783b1e5e3ae222f40a6b
Lazar Velimirovic <lazarvelimirovic43@gmail.com>
2020-09-16 16:31:27 +0200

    Implement staticically served photos

--- config/storage.config.ts
+++ config/storage.config.ts
@@ -2,6 +2,8 @@ export const StorageConfig = {
 
     photo: {
         destination: '../storage/photos/',
+        urlPrefix: '/assets/photos',
+        maxAge: 1000 * 60 * 60 * 24 * 7, //7 days
         maxSize: 1024 * 1024 * 3, //3MB in bytes
         resize: {
             thumb: {
