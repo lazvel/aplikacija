ae8fb61756d4b418e1d6d0f55673286f05d24f46
Lazar Velimirovic <lazarvelimirovic43@gmail.com>
2020-09-19 16:51:10 +0200

    Implement the filter article search mechanism

--- src/controllers/api/article.controller.ts
+++ src/controllers/api/article.controller.ts
@@ -16,6 +16,7 @@ import * as sharp from 'sharp';
 import { EditArticleDto } from "src/dtos/article/edit.article.dto";
 import { RoleCheckerGuard } from "src/misc/role.checker.guard";
 import { AllowToRoles } from "src/misc/allow.to.roles.descriptor";
+import { ArticleSearchDto } from "src/dtos/article/article.search.dto";
 
 @Controller('api/article')
 @Crud({
@@ -228,4 +229,10 @@ export class ArticleController {
         return new ApiResponse('ok', 0, 'One photo deleted!');
     }
 
+    @Post('search')
+    @UseGuards(RoleCheckerGuard)
+    @AllowToRoles('administrator', 'user')
+    async search(@Body() data: ArticleSearchDto): Promise<Article[]> {
+        return await this.service.search(data);
+    }
 }
