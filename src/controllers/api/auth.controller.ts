a0b850a96a4e9d6869510f4cb40e74dce94bcfef
Lazar Velimirovic <lazarvelimirovic43@gmail.com>
2020-09-22 00:05:37 +0200

    Implement refresh token for user

--- src/controllers/api/auth.controller.ts
+++ src/controllers/api/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body, Req, Put } from "@nestjs/common";
+/* eslint-disable prefer-const */
+import { Controller, Post, Body, Req, Put, HttpException, HttpStatus, UseGuards } from "@nestjs/common";
 import { AdministratorService } from "src/services/administrator/administrator.service";
 import { LoginAdminstratorDto } from "src/dtos/adminstrator/login.adminstrator.dto";
 import { ApiResponse } from "src/misc/api.response.class";
@@ -12,10 +13,15 @@ import { jwtSecret } from "config/jwt.secret";
 import { UserRegistrationDto } from "src/dtos/user/user.registration.dto";
 import { UserService } from "src/services/user/user.service";
 import { LoginUserDto } from "src/dtos/user/login.user.dto";
+import { JwtRefreshDataDto } from "src/dtos/auth/jwt.refresh.dto";
+import { UserRefreshTokenDto } from "src/dtos/auth/user.refresh.token.dto";
 
 @Controller('auth/')
 export class AuthController {
-    constructor(public adminstratorService: AdministratorService, public userService: UserService) {}
+    constructor(
+        public adminstratorService: AdministratorService, 
+        public userService: UserService,    
+    ) {}
 
     @Post('administrator/login') //localhost:3000/auth/administrator/login
     async doAdministratorLogin(@Body() data: LoginAdminstratorDto, @Req() req: Request): Promise<LoginInfoDto | ApiResponse> {
@@ -56,7 +62,9 @@ export class AuthController {
         const responseObject = new LoginInfoDto(
             adminstrator.administratorId,
             adminstrator.username,
-            token
+            token,
+            "",
+            "",
         );
         return new Promise(resolve => resolve(responseObject));
     }
@@ -88,13 +96,7 @@ export class AuthController {
         jwtData.role = "user";
         jwtData.id = user.userId;
         jwtData.identity = user.email;
-    
-        // eslint-disable-next-line prefer-const
-        let sada = new Date();
-        sada.setDate(sada.getDate() + 14);
-        const istekTmestamp = sada.getTime() / 1000;
-        jwtData.exp = istekTmestamp;
-
+        jwtData.exp = this.getDatePlus(60 * 5);
         jwtData.ip = req.ip.toString();
         jwtData.ua = req.headers["user-agent"];
 
@@ -102,11 +104,102 @@ export class AuthController {
         // eslint-disable-next-line prefer-const
         let token: string = jwt.sign(jwtData.toPlainObject(), jwtSecret); // potpisi jsonwebtoken
 
+        const jwtRefreshData = new JwtRefreshDataDto();
+        jwtRefreshData.role = jwtData.role;
+        jwtRefreshData.id = jwtData.id;
+        jwtRefreshData.identity = jwtData.identity;
+        jwtRefreshData.exp = this.getDatePlus(60 * 60 * 24 * 31);
+        jwtRefreshData.ip = jwtData.ip;
+        jwtRefreshData.ua = jwtData.ua;
+
+        let refreshToken: string = jwt.sign(jwtRefreshData.toPlainObject(), jwtSecret);
+
         const responseObject = new LoginInfoDto(
             user.userId,
             user.email,
-            token
+            token,
+            refreshToken,
+            this.getIsoDate(jwtRefreshData.exp),
         );
+        // ovo je malo komplikovanije moramo da izmenimo u posebnoj fji za database format niji isti kao iso
+        await this.userService.addToken(user.userId, refreshToken, this.getDatabaseDateFormat(this.getIsoDate(jwtRefreshData.exp)));
+
         return new Promise(resolve => resolve(responseObject));
+    }
+
+    @Post('user/refresh') //auth/user/refresh
+    async userTokenRefresh(@Req() req: Request, @Body() data: UserRefreshTokenDto): Promise<LoginInfoDto | ApiResponse> {
+        const userToken = await this.userService.getUserToken(data.token);
+
+        if (!userToken) {
+            return new ApiResponse("error", -10002, "No such token");
+        }
+
+        if (userToken.isValid === 0) {
+            return new ApiResponse("error", -10003, "The token is no longer valid!");
+        }
+
+        const sada = new Date();
+        const datumIsteka = new Date(userToken.expiresAt);
+    
+        if (datumIsteka.getTime() < sada.getTime()) {
+            return new ApiResponse("error", -10004, "The token has expired!");
+        }
+
+        let jwtRefreshData: JwtRefreshDataDto;
+        
+        try {
+            jwtRefreshData = jwt.verify(data.token, jwtSecret);
+        } catch (e) {
+            throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        if(!jwtRefreshData) {
+            throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        if(jwtRefreshData.ip !== req.ip.toString()) {
+            throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        if(jwtRefreshData.ua !== req.headers["user-agent"]) {
+            throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        const jwtData = new JwtDataDto();
+        jwtData.role = jwtRefreshData.role;
+        jwtData.id = jwtRefreshData.id;
+        jwtData.identity = jwtRefreshData.identity;
+        jwtData.exp = this.getDatePlus(60 * 5);
+        jwtData.ip = jwtRefreshData.ip
+        jwtData.ua = jwtRefreshData.ua;
+
+        // jwt - tajna sifra; json = {adminId, username, expire, ip, ua}
+        // eslint-disable-next-line prefer-const
+        let token: string = jwt.sign(jwtData.toPlainObject(), jwtSecret); // potpisi jsonwebtoken
+
+        const responseObject = new LoginInfoDto(
+            jwtRefreshData.id,
+            jwtRefreshData.identity,
+            token,
+            data.token,
+            this.getIsoDate(jwtRefreshData.exp),
+        );
+
+        return responseObject;
+    }
+
+    private getDatePlus(numberOfSeconds: number) {
+        return new Date().getTime() / 1000 + numberOfSeconds;
+    }
+
+    private getIsoDate(timestamp: number): string {
+        const date = new Date();
+        date.setTime(timestamp * 1000);
+        return date.toISOString();
+    }
+
+    private getDatabaseDateFormat(isoFormat: string): string {
+        return isoFormat.substr(0, 19).replace('T', ' ');
     }
 }
